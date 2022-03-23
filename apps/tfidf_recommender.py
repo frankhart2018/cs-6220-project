ad458f175a91c60ba65b3bb2f943e7c738acb4f0
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-03-23 01:23:37 -0400

    Add tfidf recommender streamlit app

---
+++ apps/tfidf_recommender.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import numpy as np
+import pandas as pd
+import pickle
+
+
+def recommend_movies_based_on_plot(movie_name, mapping, similarity_matrix):
+    if movie_name == "--Select a movie--":
+        return ""
+
+    movie_index = mapping[movie_name]
+    similarity_score = list(enumerate(similarity_matrix[movie_index]))
+    similarity_score = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    similarity_score = similarity_score[1:6]
+    movie_indices = [i[0] for i in similarity_score]
+
+    ret_str = "The movies that you might also like are: <br><br><ul>"
+    for title in df['Title'].iloc[movie_indices]:
+        ret_str += "<li>" + title + "</li>"
+    ret_str += "</ul>"
+
+    return ret_str
+
+
+st.set_page_config(
+    page_title="Content Based Filtering",
+)
+
+st.title("Content Based Filtering (TfIdf)")
+
+df = pd.read_csv("movies_filled_cleaned_processed.csv")
+
+similarity_matrix = np.load("data/similarity_matrix.npy")
+
+with open("data/df_mapping", "rb") as f:
+    df_mapping = pickle.load(f)
+
+movie_titles_tup = tuple(["--Select a movie--"] + df['Title'].tolist())
+
+movie_name = st.selectbox(
+    "Select a movie",
+    movie_titles_tup
+)
+
+components.html(recommend_movies_based_on_plot(
+    movie_name=movie_name,
+    similarity_matrix=similarity_matrix,
+    mapping=df_mapping)
+)
