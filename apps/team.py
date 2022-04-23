2f7cb1fd47469a5503fb4e7bef5b057fd645ada3
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-04-23 19:19:48 -0400

    Update aruvi info in team page

--- apps/team.py
+++ apps/team.py
@@ -15,23 +15,24 @@ components.html(
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <div class="row" style="margin-left: 10px;">
-        <div class="card" style="width: 18rem;">
+        <div class="card" style="width: 25rem;">
             <img class="card-img-top" src="https://i.ibb.co/Rp6pXB3/Whats-App-Image-2022-04-23-at-5-07-58-PM.jpg" alt="Aruvi">
             <div class="card-body">
                 <h5 class="card-title">Aruvi Puhazhendhi</h5>
-                <p class="card-text">I love machine learning and have been working with neural network based computer vision for quite some time. Apart from ML I also find compiler and programming language design very interesting and spend some time with low-level systems programming.</p>
-                <a href="https://github.com/aruvi-p" class="btn" target="_blank">
+                <p class="card-text" style="text-align: justify; text-justify: inter-word; word-break: break-all;">Masters in Artificial intelligence, former PwC senior ML associate. Specializing in Responsible AI.</p>
+                <a href="https://github.com/aruvi-p" class="btn" target="_blank" style="position: absolute; left: 0;  right: 0; bottom: 0">
                     <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style="width: 50px; height: 50px;">
                 </a>
             </div>
         </div>
 
-        <div class="card" style="width: 18rem; margin-left: 30px;">
+        <div class="card" style="width: 25rem; margin-left: 30px;">
             <img class="card-img-top" src="https://frankhart2017.github.io/portfolio/img/dp.jpg" alt="Siddhartha">
             <div class="card-body">
                 <h5 class="card-title">Siddhartha D. Choudhury</h5>
-                <p class="card-text">I love machine learning and have been working with neural network based computer vision for quite some time. Apart from ML I also find compiler and programming language design very interesting and spend some time with low-level systems programming.</p>
-                <a href="https://github.com/frankhart2018" class="btn" target="_blank">
+                <p class="card-text" style="text-align: justify; text-justify: inter-word; word-break: break-all;">MS in CS. I love machine learning and have been working with neural network based computer vision for quite some time. Apart from ML I also find compiler and programming language design very interesting and spend some time with low-level systems programming.</p>
+                <br><br>
+                <a href="https://github.com/frankhart2018" class="btn" target="_blank" style="position: absolute; left: 0;  right: 0; bottom: 0">
                     <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" style="width: 50px; height: 50px;">
                 </a>
             </div>
